74fc409a89315ddb9c66f7444842ce03d2a62a92
KiraiX77 <kiraix77@gmail.com>
2024-01-16 14:44:20 +0100

    6-list_values.sql

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,2 +1,2 @@
--- Lists all rows of the table first_table from the database hbtn_0c_0
-mysql -u Ricki -p -e "USE hbtn_0c_0; SELECT * FROM first_table;"
+-- List all rows in the table
+SELECT * FROM first_table;
